1dfc500057ecb70cc03def1ea2ff9278a802aca3
harshtarang7 <tarangharsh7@gmail.com>
2025-06-24 15:54:43 +0530

    added navbar

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./(components)/Navbar/Navbar"
+
+type Props ={}
+const DashboardWrapper =({children}:{children:React.ReactNode}) =>{
+    return(
+        <div className={'light flex bg-gray-50 text-gray-900 w-full min-h-screen'}>
+           Sidebar
+           <main className={'flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24'}>
+            <Navbar/>
+            dashboard wrapper:{children}
+           </main>
+        </div>
+    )
+}
+
+export default DashboardWrapper
